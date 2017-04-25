5c5b5ec631502d6807cf456a69db9b4f1abec0dd
Alexandre Buisine <alexandrejabuisine@gmail.com>
2017-04-25 19:20:07 +0200

    add options for restore

--- resources/mariadb_restore.sh
+++ resources/mariadb_restore.sh
@@ -4,4 +4,4 @@ PASSPHRASE=$(< "${DUPLICITY_PASSPHRASE_FILE}") \
 AWS_ACCESS_KEY_ID=$(< "${AWS_ACCESS_KEY_ID_FILE}") \
 AWS_SECRET_ACCESS_KEY=$(< "${AWS_SECRET_ACCESS_KEY_FILE}") \
 S3_USE_SIGV4="True" \
-duplicity restore --force --s3-use-new-style --s3-european-buckets "s3://s3-$AWS_REGION.amazonaws.com/$AWS_BUCKET/mariadb/" /restore/mariadb
+duplicity restore $1 --force --s3-use-new-style --s3-european-buckets "s3://s3-$AWS_REGION.amazonaws.com/$AWS_BUCKET/mariadb/" /restore/mariadb
--- resources/redis_restore.sh
+++ resources/redis_restore.sh
@@ -4,4 +4,4 @@ PASSPHRASE=$(< "${DUPLICITY_PASSPHRASE_FILE}") \
 AWS_ACCESS_KEY_ID=$(< "${AWS_ACCESS_KEY_ID_FILE}") \
 AWS_SECRET_ACCESS_KEY=$(< "${AWS_SECRET_ACCESS_KEY_FILE}") \
 S3_USE_SIGV4="True" \
-duplicity restore --s3-use-new-style --s3-european-buckets "s3://s3-$AWS_REGION.amazonaws.com/$AWS_BUCKET/redis/" /restore/redis
+duplicity restore $1 --s3-use-new-style --s3-european-buckets "s3://s3-$AWS_REGION.amazonaws.com/$AWS_BUCKET/redis/" /restore/redis
