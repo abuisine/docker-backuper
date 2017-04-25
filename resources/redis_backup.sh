10f8b1227e99c957178cb6f5554dde6e586988dc
Alexandre Buisine <alexandrejabuisine@gmail.com>
2017-04-25 20:04:48 +0200

    missed MYSQL_PASSWORD_FILE

--- resources/redis_backup.sh
+++ resources/redis_backup.sh
@@ -25,5 +25,4 @@ file_env 'AWS_SECRET_ACCESS_KEY'
 
 mkdir /tmp/redis
 cp /data/redis/appendonly.aof /tmp/redis \
-	&& S3_USE_SIGV4="True" \
-	duplicity --allow-source-mismatch --full-if-older-than 1W /tmp/redis/ --s3-use-new-style --s3-european-buckets "s3://s3-$AWS_REGION.amazonaws.com/$AWS_BUCKET/redis/"
+ && S3_USE_SIGV4="True" duplicity --allow-source-mismatch --full-if-older-than 1W /tmp/redis/ --s3-use-new-style --s3-european-buckets "s3://s3-$AWS_REGION.amazonaws.com/$AWS_BUCKET/redis/"
