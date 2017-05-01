084cc5dac8572c16eccbfaa066d54cd344c7ca66
Alexandre Buisine <alexandrejabuisine@gmail.com>
2017-05-01 11:07:25 +0200

    mr proper and solve non launching script

--- resources/redis_backup.sh
+++ resources/redis_backup.sh
@@ -23,6 +23,6 @@ file_env 'PASSPHRASE'
 file_env 'AWS_ACCESS_KEY_ID'
 file_env 'AWS_SECRET_ACCESS_KEY'
 
-mkdir /tmp/redis
+mkdir -p /tmp/redis
 cp /data/redis/appendonly.aof /tmp/redis \
- && S3_USE_SIGV4="True" duplicity --allow-source-mismatch --full-if-older-than 1W /tmp/redis/ --s3-use-new-style --s3-european-buckets "s3://s3-$AWS_REGION.amazonaws.com/$AWS_BUCKET/redis/"
+ && S3_USE_SIGV4="True" duplicity incr --allow-source-mismatch --full-if-older-than 1W /tmp/redis/ --s3-use-new-style --s3-european-buckets "s3://s3-${AWS_REGION}.amazonaws.com/${AWS_BUCKET}/redis/"
